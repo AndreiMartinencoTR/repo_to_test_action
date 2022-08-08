005787110ffa1cd1864c88ddb22c69d38b5be1e9
6118972 <a6118972@tr-thwhxk2gdt.ht.home>
2022-08-08 13:47:57 -0400

    test

--- .github/scripts/docker-entrypoint.sh
+++ .github/scripts/docker-entrypoint.sh
@@ -5,15 +5,15 @@
 # echo "::set-output name=result::$1"
 
 echo "Running inside docker"
-ls -la /etc/httpd/
+ls -la 
 
 # while :
 # do
-#     echo *** Test apache config ***
-#     /usr/sbin/httpd -t
+echo *** Test apache config ***
+/usr/sbin/httpd -t
 
-    echo *** Start apache ***
-    /usr/sbin/httpd -D FOREGROUND
+    # echo *** Start apache ***
+    # /usr/sbin/httpd -D FOREGROUND
 
 #     echo *** Restarting the server ***
 # done
